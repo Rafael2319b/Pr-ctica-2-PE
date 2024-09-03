8ee6ce4bdc638464c880d27fe90f6993c5bf086a
Rafael2319b <kiznaiver1309@gmail.com>
2024-09-03 16:33:17 -0600

    Rafael

---
+++ Getdate.js
@@ -0,0 +1,10 @@
+
+let fechaActual = new Date();
+
+let dia = fechaActual.getDate();
+
+let mes = fechaActual.getMonth();
+
+let año = fechaActual.getFullYear();
+
+console.log(`Fecha actual: ${dia}:${mes}:${año}`);
